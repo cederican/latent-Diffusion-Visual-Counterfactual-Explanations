9751241f44dd6c7a62270a3e4795045b1d6db3bc
cederican <cedimac00@icloud.com>
2024-06-01 15:55:44 +0000

    add xai lrp functionality, add working pipeline diffCounter, add folder structure

--- init_classifier.py
+++ init_classifier.py
@@ -1,137 +1,90 @@
 
 import matplotlib.pyplot as plt
-from data.dataset import *
+import PIL
+import datetime
+import os
+import numpy as np
+import wandb
 
 import torch
 import torch.nn as nn
+from torch.utils.data import DataLoader, Subset
+
 import pytorch_lightning as pl
 from pytorch_lightning.callbacks import *
 from pytorch_lightning.loggers import WandbLogger
 
-import torch.optim as optim
-from torch.utils.data import DataLoader, TensorDataset, Subset
-import numpy as np
-import wandb
-import datetime
-import os
+from diffusers import VQModel
+
+from data.dataset import *
 from init_config import CLSConfig
-import PIL
-from torchsummary import summary
 
+class VGG16Classifier(pl.LightningModule):
     
-
-class Classifier(pl.LightningModule):
-    
-    def __init__(self, input_dim, num_classes):
-        super(Classifier, self).__init__()
-
-        self.input_size = (lambda size: size[0] * size[1] * size[2])(input_dim)
-        
-        self.fc1 = nn.Sequential(
-            nn.Dropout(0.5),
-            nn.Linear(self.input_size, 1024),
-            nn.ReLU())
-        self.fc2= nn.Sequential(
-            nn.Linear(1024, num_classes))
-    """
     def __init__(self, num_classes):
-        super(Classifier, self).__init__()
-        self.layer1 = nn.Sequential(
-            nn.Conv2d(3, 64, kernel_size=3, stride=1, padding=1),
-            nn.BatchNorm2d(64),
-            nn.ReLU())
-        self.layer2 = nn.Sequential(
-            nn.Conv2d(64, 64, kernel_size=3, stride=1, padding=1),
-            nn.BatchNorm2d(64),
-            nn.ReLU(), 
-            nn.MaxPool2d(kernel_size = 2, stride = 2))
-        self.layer3 = nn.Sequential(
-            nn.Conv2d(64, 128, kernel_size=3, stride=1, padding=1),
-            nn.BatchNorm2d(128),
-            nn.ReLU())
-        self.layer4 = nn.Sequential(
-            nn.Conv2d(128, 128, kernel_size=3, stride=1, padding=1),
-            nn.BatchNorm2d(128),
-            nn.ReLU(),
-            nn.MaxPool2d(kernel_size = 2, stride = 2))
-        self.layer5 = nn.Sequential(
-            nn.Conv2d(128, 256, kernel_size=3, stride=1, padding=1),
-            nn.BatchNorm2d(256),
-            nn.ReLU())
-        self.layer6 = nn.Sequential(
-            nn.Conv2d(256, 256, kernel_size=3, stride=1, padding=1),
-            nn.BatchNorm2d(256),
-            nn.ReLU())
-        self.layer7 = nn.Sequential(
-            nn.Conv2d(256, 256, kernel_size=3, stride=1, padding=1),
-            nn.BatchNorm2d(256),
-            nn.ReLU(),
-            nn.MaxPool2d(kernel_size = 2, stride = 2))
-        self.layer8 = nn.Sequential(
-            nn.Conv2d(256, 512, kernel_size=3, stride=1, padding=1),
-            nn.BatchNorm2d(512),
-            nn.ReLU())
-        self.layer9 = nn.Sequential(
-            nn.Conv2d(512, 512, kernel_size=3, stride=1, padding=1),
-            nn.BatchNorm2d(512),
-            nn.ReLU())
-        self.layer10 = nn.Sequential(
-            nn.Conv2d(512, 512, kernel_size=3, stride=1, padding=1),
-            nn.BatchNorm2d(512),
-            nn.ReLU(),
-            nn.MaxPool2d(kernel_size = 2, stride = 2))
-        self.layer11 = nn.Sequential(
-            nn.Conv2d(512, 512, kernel_size=3, stride=1, padding=1),
-            nn.BatchNorm2d(512),
-            nn.ReLU())
-        self.layer12 = nn.Sequential(
-            nn.Conv2d(512, 512, kernel_size=3, stride=1, padding=1),
-            nn.BatchNorm2d(512),
-            nn.ReLU())
-        self.layer13 = nn.Sequential(
-            nn.Conv2d(512, 512, kernel_size=3, stride=1, padding=1),
-            nn.BatchNorm2d(512),
-            nn.ReLU(),
-            nn.MaxPool2d(kernel_size = 2, stride = 2))
-        self.fc = nn.Sequential(
-            nn.Dropout(0.5),
-            nn.Linear(4*4*512, 4096),
-            nn.ReLU())
-        self.fc1 = nn.Sequential(
-            nn.Dropout(0.5),
+        super(VGG16Classifier, self).__init__()
+        
+        self.features = nn.Sequential(
+            # Block 1
+            nn.Conv2d(3, 64, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(64, 64, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=2, stride=2),
+            
+            # Block 2
+            nn.Conv2d(64, 128, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(128, 128, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=2, stride=2),
+            
+            # Block 3
+            nn.Conv2d(128, 256, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(256, 256, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(256, 256, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=2, stride=2),
+            
+            # Block 4
+            nn.Conv2d(256, 512, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(512, 512, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(512, 512, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=2, stride=2),
+            
+            # Block 5
+            nn.Conv2d(512, 512, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(512, 512, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(512, 512, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=2, stride=2),
+        )
+        
+        self.classifier = nn.Sequential(
+            nn.Linear(512 * 8 * 8, 4096),
+            nn.ReLU(inplace=True),
+            nn.Dropout(),
             nn.Linear(4096, 4096),
-            nn.ReLU())
-        self.fc2= nn.Sequential(
-            nn.Linear(4096, num_classes))
-    """    
+            nn.ReLU(inplace=True),
+            nn.Dropout(),
+            nn.Linear(4096, num_classes),
+        )
+             
     def forward(self, x):
+        x = self.features(x)
         B, C, W, H = x.size()
         size = C * W * H
         x = x.view(-1, size)
-        x = self.fc1(x)
-        x = self.fc2(x)
+        x = self.classifier(x)
         return x
-    """
-    def forward(self, x):
-        out = self.layer1(x)
-        out = self.layer2(out)
-        out = self.layer3(out)
-        out = self.layer4(out)
-        out = self.layer5(out)
-        out = self.layer6(out)
-        out = self.layer7(out)
-        out = self.layer8(out)
-        out = self.layer9(out)
-        out = self.layer10(out)
-        out = self.layer11(out)
-        out = self.layer12(out)
-        out = self.layer13(out)
-        out = out.reshape(out.size(0), -1)
-        out = self.fc(out)
-        out = self.fc1(out)
-        out = self.fc2(out)
-        return out
-    """
+    
     def training_step(self, batch, batch_idx):
         imgs = batch['img']
         labels = batch['labels']
@@ -186,17 +139,187 @@ class Classifier(pl.LightningModule):
                 image_pil = Image.fromarray((image_array * 255).astype(np.uint8))
 
                 # Define the image filename
-                image_filename = os.path.join("/home/dai/GPU-Student-2/Cederic/DataSciPro/data/misclsData", f"{global_img_index}_{gt_misclassified}_misclassified.png")
+                image_filename = os.path.join("/home/dai/GPU-Student-2/Cederic/DataSciPro/data/misclsData_VGG16", f"{global_img_index}_{gt_misclassified}_misclassified.png")
 
                 # Save the image as a PNG file
                 image_pil.save(image_filename)
 
         images = [wandb.Image(image, caption=f"Idx: {str(global_img_index)}, gt: {str(gt_misclassified)}") for image, global_img_index, gt_misclassified in zip(imgs[wrong_indices_per_class], wrong_global_img_indexs, gt_from_misclassified)]
         wandb.log({"misclassified images": images})
+        
+        return loss
 
-        #table = wandb.Table(data=wrong_global_img_indexs, columns=["Wrong Image Indices"])
-        #self.log({"Wrong Image Indices": table})
+    def configure_optimizers(self):
+        return torch.optim.Adam(self.parameters(), lr=config.lr, weight_decay=config.weight_decay)   
 
+class VQVAEClassifier(pl.LightningModule):
+    
+    def __init__(self, num_classes):
+        super(VQVAEClassifier, self).__init__()
+
+        self.vqvae = VQModel.from_pretrained("CompVis/ldm-celebahq-256", subfolder="vqvae")
+    
+        self.fc1 = nn.Linear(3*64*64, num_classes)
+             
+    def forward(self, x):
+        z = self.vqvae.encode(x).latents
+        B, C, W, H = z.size()
+        size = C * W * H
+        z = z.view(-1, size)
+        z = self.fc1(z)
+        return z
+    
+    def training_step(self, batch, batch_idx):
+        imgs = batch['img']
+        labels = batch['labels']
+        pred = self(imgs)
+        gt = torch.where(labels > 0,
+                        torch.ones_like(labels).float(),
+                        torch.zeros_like(labels).float())
+        loss = nn.functional.binary_cross_entropy_with_logits(pred, gt)
+        self.log('loss/training', loss)
+        return loss
+    
+    def validation_step(self, batch, batch_idx):
+        imgs = batch['img']
+        labels = batch['labels']
+        pred = self(imgs)
+        gt = torch.where(labels > 0,
+                        torch.ones_like(labels).float(),
+                        torch.zeros_like(labels).float())
+        loss = nn.functional.binary_cross_entropy_with_logits(pred, gt)
+        self.log('loss/validation', loss)
+        return loss
+    
+    def test_step(self, batch, batch_idx):
+        imgs = batch['img']
+        indexs = batch['index']
+        labels = batch['labels']
+        pred = self(imgs)
+        gt = torch.where(labels > 0,
+                        torch.ones_like(labels).float(),
+                        torch.zeros_like(labels).float())
+        loss = nn.functional.binary_cross_entropy_with_logits(pred, gt)
+
+        # Calculate accuracy
+        preds_binary = torch.sigmoid(pred) > 0.5
+        correct = torch.sum(preds_binary == gt)
+        accuracy = correct.item() / torch.numel(gt)
+
+        # Get the wrong classified images per class
+        wrong_indices = torch.nonzero(preds_binary != gt, as_tuple=True)
+        wrong_indices_per_class = wrong_indices[0][wrong_indices[1] == CelebHQAttrDataset.cls_to_id['Smiling']] #Look up in the id_to_cls from dataset
+        gt_from_misclassified = [gt[wrong_indices_per_class][i][CelebHQAttrDataset.cls_to_id['Smiling']].item() for i in range(len(wrong_indices_per_class))]
+        wrong_global_img_indexs = indexs[wrong_indices_per_class]
+
+        self.log('loss/testing', loss)
+        self.log('accuracy/testing', accuracy)
+
+        for image, global_img_index, gt_misclassified in zip(imgs[wrong_indices_per_class], wrong_global_img_indexs, gt_from_misclassified):
+            # Convert tensor to numpy array and then to PIL Image
+            if global_img_index.item() < 30000:
+                image_tensor = (image + 1) / 2
+                image_array = image_tensor.permute(1, 2, 0).cpu().numpy()
+                image_pil = Image.fromarray((image_array * 255).astype(np.uint8))
+
+                # Define the image filename
+                image_filename = os.path.join("/home/dai/GPU-Student-2/Cederic/DataSciPro/data/misclsData_VQVAE", f"{global_img_index}_{gt_misclassified}_misclassified.png")
+
+                # Save the image as a PNG file
+                image_pil.save(image_filename)
+
+        images = [wandb.Image(image, caption=f"Idx: {str(global_img_index)}, gt: {str(gt_misclassified)}") for image, global_img_index, gt_misclassified in zip(imgs[wrong_indices_per_class], wrong_global_img_indexs, gt_from_misclassified)]
+        wandb.log({"misclassified images": images})
+        
+        return loss
+
+    def configure_optimizers(self):
+        return torch.optim.Adam(self.parameters(), lr=config.lr, weight_decay=config.weight_decay)   
+
+class LinearClassifier(pl.LightningModule):
+    
+    def __init__(self, input_dim, num_classes):
+        super(LinearClassifier, self).__init__()
+
+        self.input_size = (lambda size: size[0] * size[1] * size[2])(input_dim)
+        
+        self.fc1 = nn.Sequential(
+            nn.Dropout(0.5),
+            nn.Linear(self.input_size, 1024),
+            nn.ReLU())
+        self.fc2= nn.Sequential(
+            nn.Linear(1024, num_classes))
+   
+    def forward(self, x):
+        B, C, W, H = x.size()
+        size = C * W * H
+        x = x.view(-1, size)
+        x = self.fc1(x)
+        x = self.fc2(x)
+        return x
+    
+    def training_step(self, batch, batch_idx):
+        imgs = batch['img']
+        labels = batch['labels']
+        pred = self(imgs)
+        gt = torch.where(labels > 0,
+                        torch.ones_like(labels).float(),
+                        torch.zeros_like(labels).float())
+        loss = nn.functional.binary_cross_entropy_with_logits(pred, gt)
+        self.log('loss/training', loss)
+        return loss
+    
+    def validation_step(self, batch, batch_idx):
+        imgs = batch['img']
+        labels = batch['labels']
+        pred = self(imgs)
+        gt = torch.where(labels > 0,
+                        torch.ones_like(labels).float(),
+                        torch.zeros_like(labels).float())
+        loss = nn.functional.binary_cross_entropy_with_logits(pred, gt)
+        self.log('loss/validation', loss)
+        return loss
+    
+    def test_step(self, batch, batch_idx):
+        imgs = batch['img']
+        indexs = batch['index']
+        labels = batch['labels']
+        pred = self(imgs)
+        gt = torch.where(labels > 0,
+                        torch.ones_like(labels).float(),
+                        torch.zeros_like(labels).float())
+        loss = nn.functional.binary_cross_entropy_with_logits(pred, gt)
+
+        # Calculate accuracy
+        preds_binary = torch.sigmoid(pred) > 0.5
+        correct = torch.sum(preds_binary == gt)
+        accuracy = correct.item() / torch.numel(gt)
+
+        # Get the wrong classified images per class
+        wrong_indices = torch.nonzero(preds_binary != gt, as_tuple=True)
+        wrong_indices_per_class = wrong_indices[0][wrong_indices[1] == CelebHQAttrDataset.cls_to_id['Smiling']] #Look up in the id_to_cls from dataset
+        gt_from_misclassified = [gt[wrong_indices_per_class][i][CelebHQAttrDataset.cls_to_id['Smiling']].item() for i in range(len(wrong_indices_per_class))]
+        wrong_global_img_indexs = indexs[wrong_indices_per_class]
+
+        self.log('loss/testing', loss)
+        self.log('accuracy/testing', accuracy)
+
+        for image, global_img_index, gt_misclassified in zip(imgs[wrong_indices_per_class], wrong_global_img_indexs, gt_from_misclassified):
+            # Convert tensor to numpy array and then to PIL Image
+            if global_img_index.item() < 28000:
+                image_tensor = (image + 1) / 2
+                image_array = image_tensor.permute(1, 2, 0).cpu().numpy()
+                image_pil = Image.fromarray((image_array * 255).astype(np.uint8))
+
+                # Define the image filename
+                image_filename = os.path.join("/home/dai/GPU-Student-2/Cederic/DataSciPro/data/misclsData_LinearCls", f"{global_img_index}_{gt_misclassified}_misclassified.png")
+
+                # Save the image as a PNG file
+                image_pil.save(image_filename)
+
+        images = [wandb.Image(image, caption=f"Idx: {str(global_img_index)}, gt: {str(gt_misclassified)}") for image, global_img_index, gt_misclassified in zip(imgs[wrong_indices_per_class], wrong_global_img_indexs, gt_from_misclassified)]
+        wandb.log({"misclassified images": images})
+        
         return loss
 
     def configure_optimizers(self):
@@ -226,16 +349,27 @@ def trainCLS(
     print("Validation set size:", len(val_dataset))
     print("Test set size:", len(test_dataset))
     
-    model = Classifier(data[0]['img'].shape, num_cls)
-    #model = Classifier(num_cls)
-    #summary(model, 256)
+    if config.architecture == 'linear':
+        model = LinearClassifier(data[0]['img'].shape, num_cls)
+        #model.to(config.devices)
+    elif config.architecture == 'vqvae':
+        #vqvae.to(config.devices)
+        model = VQVAEClassifier(num_cls)
+        #model.to(config.devices)
+    elif config.architecture == 'vgg16':
+        #vqvae.to(config.devices)
+        model = VGG16Classifier(num_cls)
+        #model.to(config.devices)
+    else:
+        print("Sorry, model architecture not implemented!")
+
     wandb.log({'total_parameters': count_parameters(model)})
     
     current_time = datetime.datetime.now()
     formatted_time = current_time.strftime("%Y-%m-%d %H:%M:%S")
     ckpt_callback = ModelCheckpoint(
-        dirpath="/home/dai/GPU-Student-2/Cederic/pjds_group8/cls_checkpoints",
-        filename="ffhq256." + formatted_time,
+        dirpath="/home/dai/GPU-Student-2/Cederic/DataSciPro/cls_checkpoints",
+        filename="ffhq256." + "b"+ str(config.batch_size) + config.architecture + formatted_time,
         monitor="loss/validation",
         mode="min",
         save_top_k=1,
@@ -261,7 +395,16 @@ def trainCLS(
     
 def main(config, data, num_cls):
     
-    wandb.init(name=config.run_name, project="DataSciPro", entity="cedimac00", config=config)
+    wandb_config = {
+    "run_name": config.run_name,
+    "architecture": config.architecture,
+    "batch_size": config.batch_size,
+    "epochs": config.epochs,
+    "lr": config.lr,
+    "weight_decay": config.weight_decay,
+    "devices": config.devices,
+}
+    wandb.init(name=config.run_name, project="DataSciPro", entity="cedimac00", config=wandb_config)
     best_model_path = trainCLS(config, data, num_cls)
     print(best_model_path)
     wandb.finish()
--- init_config.py
+++ init_config.py
@@ -1,8 +1,9 @@
 class CLSConfig():
-    run_name: str = 'DiffAECLS-05.28-Linear'
-    batch_size: int = 128
+    run_name: str = 'DiffAECLS-06.01-VGG16'
+    architecture: str = 'vgg16' # or 'linear' or vgg16
+    batch_size: int = 32
     epochs: int = 2
     lr: float = 1e-4
     weight_decay: float = 0.01
     devices: list = [0]
-    #input_dim: tuple = (256,256,256)
+    
